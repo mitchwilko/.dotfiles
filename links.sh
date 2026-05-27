e1b2a4fad0c162fe9b5f571664e5abe5e1547878
mitch <mitchell01wilkinson@gmail.com>
2026-05-27 15:07:01 +1000

    fixed links file

--- links.sh
+++ links.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-ln -nsf ./.dotfiles/vimrc ~/.vimrc
-ln -nsf ./.dotfiles/vim ~/.vim
+ln -sf ~/.dotfiles/vimrc ~/.vimrc
+ln -sf ~/.dotfiles/vim ~/.vim
 
-ln -nsf ./.dotfiles/condarc ~/.condarc
-ln -nsf ./.dotfiles/conda ~/.conda
+ln -sf ~/.dotfiles/condarc ~/.condarc
+ln -sf ~/.dotfiles/conda ~/.conda
 
-ln -nsf ./.dotfiles/zshrc ~/.zshrc
-ln -nsf ./.dotfiles/bashrc ~/.bashrc
+ln -nsf ~/.dotfiles/zshrc ~/.zshrc
+ln -nsf ~/.dotfiles/bashrc ~/.bashrc
